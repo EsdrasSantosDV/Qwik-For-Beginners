758a37044c740cf65c4d2a930da491916d41df0a
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-03-03 22:07:34 -0300

    feat:property drilling

--- src/routes/stores/index.tsx
+++ src/routes/stores/index.tsx
@@ -1,44 +1,74 @@
 import { component$, $, useStore } from "@builder.io/qwik";
 
-/*
-O sistema de renderização reativa rápida possui recursos de renderização de granulação fina, portanto,
- é possível atualizar
-apenas um elemento do template diretamente no DOM sem ter que chamar toda a renderização do componente função.
-Como você pode ver, há um mecanismo de otimização em jogo aqui.Qwik está tentando evitar ao máximo chamar a função de renderização completa.
-Se houver uma maneira de atualizar o DOM diretamente de maneira refinada, o Qwik fará isso e é
-vai evitar uma renderização completa do componente.
- */
+//ANTES DE EXPLICAR CONTEXT
+//VOU EXPLICAR O CONCEITO DE Property Drilling
+
+
+
+
+interface MessagesStore {
+  messages: string[];
+  index: number;
+}
+
 export default component$(() => {
   const messages = ["Hwllo", "Welcome", "Learn ANgular"];
 
   const store = useStore({
+    messages,
     index: 0,
   });
 
-  /*
-  Você não precisa pensar como um desenvolvedor de aplicativos sobre como o
-   sistema de renderização funciona enquanto você está desenvolvendo seu aplicativo.
-    A única coisa que você deve lembrar é armazenar todos os
-    seus dados que o componente usa dentro do
-    store e quick saberão exatamente quando renderizar novamente seu componente.
-   */
 
-  //SE VOCE VER O LOG, O OBJETO E UM OBJETO PROXY
-    //E UM OBJETO PROXY, E ESXAMENTTE UM OBJETO MUTAVEL NORMAL
-    //COM A FUNCIONALIDADE ADICIONAL QUE QUALQUER UM  PODE ASSINAR ALTERAÇOES NAS PROPRIEDADES DESSE PROXY
-
-    //COM O ISSO O QWIK SABE EXATAMENTE O QUE PRECISA SER MUDADO NO COMPONENTE
-  console.log(store);
   return (
     <>
       <h1>Qwik Stores:Index da Store: {store.index}</h1>
-
-      <h3>{messages[store.index]}</h3>
-
-      {/*  O QWIK NÃO VAI CHAMAR TODA A REDERIZAÇÃO DESSE COMPONENTE EM VARIOS CASOS*/}
-
-      {/*  O QWIK USA UM SISTEMA DE RENDEZIAÇÃO REATIVA SOB O CAPO BASEADO EM PROXIS*/}
+      {/*
+      BELEZA AQUI TEMOS UMA RELAÇÃO DE PAI PRA FILHO
+      MAS VAMOS PENSAR EM UMA APLICAÇÃO MUITO  MAIS COMPLEXA QUE JA VIMOS
+      ONDE VOCE TEM UM ARVORE DE COMPONENTES COM MUITOS NIVEIS DE PROFUNDIDADE
+      E QUE ESSSA APLICAÇÃO TENHA DADODS NORMALMENTE GLOBAIS, POR EXEMPLO
+      ESTADO DE APLICAÇÃO, UM EXEMPLO CLARO E A ROLE DO USUARIO
+      TODAS ESSA INFORMAÇÃO GLOBAL E UTILIZADA EM MUITOS LOCAIS DIFERENTES DA APLICAÇÃO
+      NUM CASO ONDE NÃO UTILIZAMOS O CONTEXTO, TEMOS UMA PERFURAÇÃO DE PROPRIEDADE
+      PQ PREICSMAOS PASSAR POR VARIOS NIVEIS DE PROFUNDIDADE PARA APENAS
+      UTILIZAR EM UM RAMO FOLHA, E NESSAS PERFURAÇOES DE NIVEIS
+      VARIOS NIVEIS NÃO ESTÃO USANDO ESSES DADOS
+      */}
+      <MessagePage store={store}></MessagePage>
       <button onClick$={() => store.index++}>Next message</button>
     </>
   );
 });
+
+interface MessageProps {
+  store: MessagesStore;
+}
+
+export const MessagePage = component$((props: MessageProps) => {
+    //E UM CASO TIPICO DE PERFURAÇÃO DE PROPRIEDCADE, SE NÃO TIVESSE IMPLEMENTANDO ALGO RELAICONADO
+    //A STORE
+    /*
+    não estão usando os dados, eles estão apenas passando as propriedades
+    para baixo na árvore de componentes, para
+    que outro componente aninhado mais profundamente use os dados.
+     */
+  return (
+    <>
+
+        <MessageContainer store={props.store}></MessageContainer>
+    </>
+  );
+});
+
+
+export const MessageContainer = component$((props:MessageProps)=>{
+    const { messages, index } = props.store;
+
+    //PODEMOS AOGRA CRIAR NOSSO PRIMEIRO CONTEXTO NO PROXIMO COMMIT
+    return (
+        <>
+            <h3>{messages[index]}</h3>
+        </>
+    );
+})
