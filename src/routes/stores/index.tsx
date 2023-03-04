b408278a05ddc062356c9cfbc895f7dc7c80b610
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-03-03 21:00:14 -0300

    feat:first store in qwik

--- src/routes/stores/index.tsx
+++ src/routes/stores/index.tsx
@@ -1,25 +1,35 @@
-import { component$, $ } from "@builder.io/qwik";
+import { component$, $, useStore } from "@builder.io/qwik";
 import { HelloMessage } from "~/components/header-message/hello-message";
 
+/*
+Quick nos permite renderizar novamente os modelos dos componentes que
+ mudam apenas quando os dados que eles
+uso também mudou.
+Isso significa que, em uma aplicação rápida, vamos renderizar novamente apenas as partes da página que
+absolutamente necessário.
+ */
 export default component$(() => {
   const messages = ["Hwllo", "Welcome", "Learn ANgular"];
 
-  let index = 0; //Há algum estado acontecendo aqui que é representado aqui pela propriedade index.
+  //Uma loja é um espaço reservado onde os dados de um componente devem ser mantidos
 
-  /*
-    Essas funções de renderização devem ser chamadas apenas uma vez quando o componente 
-    for renderizado, na primeira vez  
-    que a página é carregada e então ela só deve ser chamada novamente se a
-     */
+  const store = useStore({
+    index: 0,
+  });
+  //Portanto, o objeto store funciona exatamente como um objeto JavaScript simples.
+  const obj={
+      index:0
+  }
+  //ESSE OBJETO TEM O MESMO COMPORTAMENTO DO OBJETO DA STORE
+  //PODEMOS ACESSAS SUAS PROPRIEDADES ATE NO TEMPLATE
   return (
     <>
-      {/*ISSO IRIA FUNCIONAR SE TIVESSE NO ANGULAR POR EXEMPLO, PQ E COMPATIVEL COM O ESTADO MUTAVEL*/}
-      <h1>Qwik Stores</h1>
+      <h1>Qwik Stores:Index da Store: {store.index}</h1>
 
-      {/*MAS O QWIK NÃO FICA COMPARANDO A ARVORE DE COMPONENTE INTEIRA PRA VER SE TEVE MUDANÇAS*/}
-      <h3>{messages[index]}</h3>
-      {/*VERIFA*/}
-      <button onClick$={() => index++}>Next message</button>
+      <h3>{messages[store.index]}</h3>
+
+      {/*Como você pode ver, você pode modificar as propriedades deste objeto de armazenamento.*/}
+      <button onClick$={() => store.index++}>Next message</button>
     </>
   );
 });
