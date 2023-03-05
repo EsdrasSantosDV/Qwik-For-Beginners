d3fa61da53497027005a3a3f326e9f8f25faf1aa
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-03-04 21:29:09 -0300

    feat:resource component value, onResolved

--- src/routes/resources/index.tsx
+++ src/routes/resources/index.tsx
@@ -11,39 +11,31 @@ interface CoursesStore {
   courses: Course[];
 }
 export default component$(() => {
-  //OS DAODS VAO SER CARREGADOS NO SERVIDOR NA PRIMEIRA VEZ
-  //QUE O COMPONENTE FOR RENDERIZADO E AI SIM
-  //OS DADOS VAÃO SER EXBIDISO USANDO  O RESOURCE
 
-  //PRECISAMOS USAR UM GENERIC PARAMETER DO TIPO QUE
-  //QUEREMOS RECBER DO BACKEND QUE AQUI SERIA UMA LISTA DE COURSOS
   const resource = useResource$<Course[]>(async () => {
-    //ESSE RESOURCE VAI RETORNAR UMA LISTA DE CURSOS DO BACK
+
     return getCourses();
   });
-  //EM SEGUIDA O APLICATIVO SERA RETOMADO NO LADO DO CLIENTE
-  //aqui temos OS VALORES DO RESOURCE
-  // E UM FLAG SE O RECURSO ESTA AINDA CARREGANDO OU NÃO
 
-  //LEMRBA QUE O RESOURCE E UM HOOK, ELA TEM MEMORIA
-  //TEM COMPORTAMENTO E TEM O CONTEXTO DE ONDE E CHAMADO
-
-  /*
-    Por outro lado, se esse componente for carregado lentamente no front-end, o recurso do usuário será
-    saber que quando o componente é inicializado no front-end, ele não precisa chamar o comando get
-    Os dados já foram carregados na renderização inicial que recebemos do servidor, então a função
-    conhece o contexto em que está sendo usado e se comportará de maneira diferente nas chamadas para o
-    função de renderização de componentes.
-     */
 
   return (
     <>
+    {/*    O QWIK TEM UM COMPONENTE QUE AJUDA A GENTE DEMAIS COM O RESOURCES*/}
+    {/* APRIMEIRA PROPS E O PROPRIO RESOURCE A SER UTILZIADO  */}
+    {/*  TEMSO EM SEGUIDA UMA FUNCAO DE MANIPULAÇÃO DOS EVENTOS NÃO RESOLVIDOS QUE VAI SER ACIONADA*/}
+    {/*    SEMPRE QUE OS DADSO FOREM RESOLVIDOS COM SUCESSO*/}
+    {/*    O ONRESOLVED VAI SER CHAMADO QUANDO RECEBEMOS OS DADOS*/}
+        <Resource value={resource} onResolved={courses=>(
+        <>
+            {
+                courses.map(course=>(
+                 <h3>{course.description}</h3>
+                ))
+            }
+        </>
+        )
+        }/>
 
-      {/*  SE OLHARMOS NAS REQUISICOES AJAX NÃO VEREMOS NENHUMA REQUISIÇÃO LA, PQ TUDO FOI JA PUXADO NO SERVIDOR DO FRONT*/}
-
-      {resource.value.then((courses) =>
-        courses.map((course) => <h3>{course.description}</h3>)
-      )}
     </>
   );
 });
